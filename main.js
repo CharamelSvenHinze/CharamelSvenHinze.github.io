f529f210cbdbe3f0978e6d9bfd6075cb655ed124
hinze_charamel <hinze@charamel.com>
2023-05-02 13:52:44 +0200

    add video

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 let app = new PIXI.Application({width: 640, height:360, backgroundAlpha: 0});
 document.body.appendChild(app.view);
 
-let texture = PIXI.Texture.from("video.webm")
+let sprite = PIXI.Sprite.from("Lena.png");
+sprite.width = app.screen.width;
+sprite.height = app.screen.height;
+app.stage.addChild(sprite);
 
-let video = new PIXI.Sprite(texture)
+let texture = PIXI.Texture.from("video.webm");
+
+let video = new PIXI.Sprite(texture);
 app.stage.addChild(video);
