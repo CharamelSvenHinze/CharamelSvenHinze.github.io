74a154eaab047c4620a2a7606227d7e64fe7f1c4
hinze_charamel <hinze@charamel.com>
2023-05-02 13:44:22 +0200

    pixi init

---
+++ main.js
@@ -0,0 +1,7 @@
+import * as PIXI from "pixi.js"
+
+let app = new PIXI.Application({width: 640, height:360});
+document.body.appendChild(app.view);
+
+let sprite = PIXI.Sprite.from("Lena.png");
+app.stage.addChild(sprite);
